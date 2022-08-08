3cc1a01d357f6a73ffdaf9a8eca82d264e55a107
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-08-08 22:44:47 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,8 @@
+const startSwitchColor = document.querySelector('button[data-start]');
 
+
+console.log(startSwitchColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
