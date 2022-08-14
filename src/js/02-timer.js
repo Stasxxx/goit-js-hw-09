614e3affcbbb997faca6e56c9326529fe8964ec1
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-08-14 18:37:18 +0300

    end 2 task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -12,12 +12,9 @@ const refs = {
   minutes: document.querySelector('span[data-minutes]'),
   seconds: document.querySelector('span[data-seconds]'),
 }
-// console.log(refs.days);
 
 refs.startBtn.setAttribute('disabled', '');
-// input.addEventListener('input', flatpickr)
-// let selectedDate = Date.now(input.value);
-// console.log(selectedDate)
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -40,6 +37,7 @@ flatpickr(refs.input, options);
 
 class Timer {
   constructor({ onTick }) {
+    this.timerId = null;
     this.isActive = false;
     this.onTick = onTick;
   }
@@ -48,30 +46,24 @@ class Timer {
     if (this.isActive) {
       return;
     }
+
     this.isActive = true;
     
-    setInterval(() => {
+    this.timerId =setInterval(() => {
       const currentTiem = Date.now();
       const deltaTime = selectedDate - currentTiem;
-      const time = convertMs(deltaTime);
-      // console.log(time);
+      const time = this.convertMs(deltaTime);
+      console.log(deltaTime);
       this.onTick(time);
-      // updateClockface(time)
+
+      if (deltaTime <= 1000) {
+        clearInterval(this.timerId);
+      }
+      
     }, 1000);
   }
-}
 
-const timer = new Timer({
-  onTick: updateClockface,
-});
-
-
-
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
-
-function convertMs(ms) {
+  convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
@@ -79,35 +71,32 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = pad(Math.floor(ms / day));
+  const days = this.addLeadingZero(Math.floor(ms / day));
   // Remaining hours
-  const hours = pad(Math.floor((ms % day) / hour));
+  const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  const minutes = this.addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+  const seconds = this.addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
+  }
+  
+  addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
 }
 
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-
-// const clock = new Timer({
-//   onTick: updateClockface,
-// });
-
-
-refs.startBtn.addEventListener('click', () => {
-  timer.start();
+const timer = new Timer({
+  onTick: updateClockface,
 });
-// // refs.startBtn.addEventListener('click', timer.start.bind(timer));
-// // refs.stopBtn.addEventListener('click', timer.stop.bind(timer))
+
+
 function updateClockface({ days, hours, minutes, seconds }) {
   refs.days.textContent = `${days}`;
   refs.hours.textContent = `${hours}`;
   refs.minutes.textContent = `${minutes}`;
   refs.seconds.textContent = `${seconds}`;
 }
+
+refs.startBtn.addEventListener('click', timer.start.bind(timer));
